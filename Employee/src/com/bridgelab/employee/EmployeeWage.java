af81fe3a0db5d5cbf18396c1984a745bf3b1cf40
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-06-16 17:14:50 +0530

    Employee is Present or absent

--- Employee/src/com/bridgelab/employee/EmployeeWage.java
+++ Employee/src/com/bridgelab/employee/EmployeeWage.java
@@ -3,5 +3,14 @@ package com.bridgelab.employee;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation");
+        int IS_FULL_TIME=1;
+        double empCheck=Math.floor(Math.random()*10)%2;
+
+        if (empCheck==IS_FULL_TIME){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }
